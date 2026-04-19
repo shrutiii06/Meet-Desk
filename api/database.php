8206a2ed9f95f742d6417c811eea8235321b5862
shrutiii06 <shrutivaishnani@gmail.com>
2026-04-19 19:38:17 +0530

    Update MongoDB URI definition to use environment variable

--- api/database.php
+++ api/database.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * MONGODB DATABASE CONFIGURATION
- * 
- * This file defines the MongoDB connection string
- * All backend files use this to connect to MongoDB
- */
-
 if (!defined('MONGODB_URI')) {
-    // Local MongoDB connection (Running on Port 27017)
-    define('MONGODB_URI', 'mongodb://localhost:27017');
-    
-    // Note: Database name is 'meetdesk'
-    // Note: Collection name is 'users'
+    define('MONGODB_URI', getenv('MONGODB_URI') ?: 'mongodb://localhost:27017');
 }
